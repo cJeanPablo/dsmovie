426057ad1a7e89b1a562498d787dcb4caf4d4ec3
cJeanPablo <cjeanpablo@gmail.com>
2022-03-11 12:48:42 -0300

    correção de código

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -7,7 +7,7 @@ function Navbar() {
         <nav className="container">
           <div className="dsmovie-nav-content">
             <h1>DSMovie</h1>
-            <a href="https://github.com/cjeanpablo" target="_blank">
+            <a href="https://github.com/cjeanpablo" target="_blank" rel="noreferrer">
               <div className="dsmovie-contact-container">
                 <GitHubIcon target="_blank"/>
                 <p className="dsmovie-contact-link">/devsuperior</p>
